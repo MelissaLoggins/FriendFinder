d902e1104578fbed9780b299f3a84de56b26613f
MelissaLoggins <melissaloggins@gmail.com>
2017-03-07 15:58:50 -0500

    Data files complete; route files in progress.

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friends = [
+  {
+  	"name":"Melissa",
+  	"photo":"http://images.8tracks.com/cover/i/002/886/800/made_you_look-9609.png?rect=0,0,500,500&q=98&fm=jpg&fit=max",
+  	"scores":[
+	    2,
+	    4,
+	    5,
+        3,
+	    2,
+	    3,
+	    3,
+	    2,
+	    4,
+	    1
+    ]
+  },
+  {
+  	"name":"Mikey",
+  	"photo":"https://upload.wikimedia.org/wikipedia/en/e/e1/ToniBasilMickey.png",
+  	"scores":[
+	    5,
+	    1,
+	    4,
+        4,
+	    5,
+	    1,
+	    2,
+	    5,
+	    4,
+	    1
+    ]
+  },
+  {
+  	"name":"Danny",
+  	"photo":"http://images.45cat.com/kenny-loggins-with-jim-messina-dannys-song-cbs-2.jpg",
+  	"scores":[
+	    4,
+	    2,
+	    3,
+        5,
+	    3,
+	    2,
+	    4,
+	    3,
+	    3,
+	    2
+    ]
+  },
+  {
+  	"name":"John",
+  	"photo":"http://s3.amazonaws.com/production.mediajoint.prx.org/public/piece_images/311764/Big_John_painting_by_Mystela_small.jpg",
+  	"scores":[
+	    3,
+	    4,
+	    1,
+        2,
+	    1,
+	    5,
+	    3,
+	    1,
+	    2,
+	    5
+    ]
+  },
+  {
+  	"name": "Jenny",
+	"photo": "https://1.bp.blogspot.com/-5UJyz4CF5Es/VFLlkPizG-I/AAAAAAAACDY/roT21_XJC6Q/s1600/8675309.png",
+	"scores": [
+	 	4,
+		2,
+		5,	
+		1,
+		3,
+		2,
+		2,
+		1,
+		3,
+		2
+	]
+},
+{
+	"name": "Billie Jean",
+	"photo": "https://i.ytimg.com/vi/B02LXuKWGTI/hqdefault.jpg",
+	"scores": [
+		5,
+		2,
+		2,
+		2,
+		4,
+		1,
+		3,
+		2,
+		5,
+		5
+	]
+},
+{
+	"name": "Angie",
+	"photo": "https://upload.wikimedia.org/wikipedia/en/f/f5/Angie1.jpg",
+	"scores": [
+		3,
+		3,
+		4,
+		2,
+		2,
+		1,
+		3,
+		2,
+		2,
+		3
+	]
+},
+{
+	"name": "Fancy",
+	"photo": "http://www.azquotes.com/picture-quotes/quote-i-might-have-been-born-just-plain-white-trash-but-fancy-was-my-name-reba-mcentire-63-62-05.jpg",
+	"scores": [
+		4,
+		3,
+		4,
+		1,
+		5,
+		2,
+		5,
+		3,
+		1,
+		4
+	]
+},
+{
+  	"name":"Bert",
+  	"photo":"https://www.jerrysguitarbar.com/wp-content/uploads/2016/08/Donovan-Berts-Blues-Songsheet-Optimized.png",
+  	"scores":[
+	    4,
+	    2,
+	    3,
+        3,
+	    1,
+	    5,
+	    4,
+	    1,
+	    2,
+	    5
+    ]
+},
+{
+	"name": "Haushinka",
+	"photo": "http://images6.fanpop.com/image/forum/171000/171336_1352103977409_full.jpg",
+	"scores": [
+		1,
+		5,
+		3,
+		2,
+		3,
+		3,
+		4,
+		3,
+		5,
+		1
+	]
+  }
+	
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,26 +1,19 @@
-var bodyParser = require('body-parser')
-var express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
- 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+
  
 // parse application/json 
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
