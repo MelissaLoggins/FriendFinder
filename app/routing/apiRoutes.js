116205ff88a42944f637375df468cf751090184b
MelissaLoggins <melissaloggins@gmail.com>
2017-03-11 15:16:43 -0500

    Modal html coded, but ajax is not sending the input.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,31 @@
-var friends = require('../data/friends.js');
+var friendData = require('../data/friends.js');
 
 module.exports = function (app) {
 
 	app.get('/api/friends', function (req, res) {
-		res.json(friends);
+		res.json(friendData);
 	});
-}
+
+	app.post("/api/friends", function(req, res) {
+
+		var newUser = req.body;
+		console.log(newUser);
+	
+		friendData.push(newUser);
+
+		var scoreDifference = 0;
+		var totalDifference = [];
+
+		for (var i = 0; i < (friendData.length - 1); i++){
+			for (var j = 0; j < friendData[i].scores.length; j++){
+				scoreDifference += Math.abs(friendData[i].scores[j] - newUser.scores[j]);
+			}
+
+			totalDifference.push(scoreDifference);
+			scoreDifference = 0;
+		}
+
+			var friendResult = friendData[totalDifference.indexOf(Math.min.apply(null, totalDifference))];
+			res.send(friendResult);
+		});
+	}
